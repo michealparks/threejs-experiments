9650dcf8b16bbb498397073d8f57c1838332c92a
Micheal Parks <michealparks1989@gmail.com>
2020-08-18 15:07:49 -0400

    tweaks

---
+++ pages/js/constants.js
@@ -0,0 +1,6 @@
+export const LIGHT_COLORS = {
+  cool: 0xFFFFFF,
+  warm: 0xFFF5b6,
+  warmer: 0xEFC070,
+  warmest: 0xE47025
+}
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
 const app = express()
+const port = 5000
 app.use(express.static('pages'))
-app.listen(5000)
+app.listen(port, () => console.log(`Listening on port ${port}.`))
