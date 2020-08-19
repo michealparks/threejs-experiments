9a22215c24e0fdb6af25a29bc9d4fd3fa3d7db7c
Micheal Parks <michealparks1989@gmail.com>
2020-08-19 17:56:48 -0400

    scene graph

--- pages/js/core.js
+++ pages/js/core.js
@@ -1,4 +1,6 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@v0.119.0/build/three.module.js'
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@v0.119.0/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@v0.119.0/examples/jsm/loaders/GLTFLoader.js'
 import { COLORS } from './constants.js'
 
 const createRenderer = (canvas) => {
@@ -20,11 +22,12 @@ const createRenderer = (canvas) => {
   return renderer
 }
 
-const createCamera = () => {
-  const fov = 75
-  const aspect = 2
-  const near = 0.01
-  const far = 10
+const createCamera = ({
+  fov = 75,
+  aspect = 2,
+  near = 0.01,
+  far = 10
+}) => {
   return new THREE.PerspectiveCamera(fov, aspect, near, far)
 }
 
@@ -43,9 +46,13 @@ const renderToDisplaySize = (canvas, renderer, scene, camera) => {
   renderer.render(scene, camera)
 }
 
-export const createCore = ({ canvas, ambientLight = 0.6 }) => {
+export const createCore = ({
+  canvas,
+  camera: cameraConfig = {},
+  ambientLight = 0.6
+}) => {
   const renderer = createRenderer(canvas)
-  const camera = createCamera()
+  const camera = createCamera(cameraConfig || {})
   const scene = new THREE.Scene()
 
   const render = (fn, time) => {
@@ -56,7 +63,6 @@ export const createCore = ({ canvas, ambientLight = 0.6 }) => {
   const setAnimationLoop = (fn) => {
     renderer.setAnimationLoop(fn === undefined ? null : render.bind(undefined, fn))
   }
-
   if (ambientLight > 0.0) {
     const light = new THREE.AmbientLight(COLORS.warmLight, ambientLight)
     scene.add(light)
@@ -68,4 +74,18 @@ export const createCore = ({ canvas, ambientLight = 0.6 }) => {
     scene,
     setAnimationLoop
   }
+}
+
+export const createOrbitControls = ({ renderer, camera, rotate = true }) => {
+  const controls = new OrbitControls(camera, renderer.domElement)
+  controls.enableDamping = true
+  controls.dampingFactor = 0.05
+  controls.autoRotate = rotate
+  return controls
+}
+
+const loader = new GLTFLoader()
+export const loadGLTF = async (src) => {
+  const model = await loader.loadAsync(src)
+  return model
 }
