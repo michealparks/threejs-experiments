86479510b21dbea69a74b588d883e202796a1c30
Micheal Parks <michealparks1989@gmail.com>
2020-08-19 00:05:01 -0400

    html cleanup

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 5000
-app.use(express.static('pages'))
+const staticDir = express.static('pages')
+app.use('/threejs-experiments', staticDir)
+app.use(staticDir)
 app.listen(port, () => console.log(`Listening on port ${port}.`))
