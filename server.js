332ec45558f954a725890c606d02e697c005b3ef
Micheal Parks <michealparks1989@gmail.com>
2020-08-19 14:11:00 -0400

    cleanup donut

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 5000
+
+const open = (url) => {
+  const start = (process.platform === 'darwin' ? 'open' : process.platform === 'win32' ? 'start' : 'xdg-open')
+  require('child_process').exec(`${start} ${url}`)
+}
+
 const staticDir = express.static('pages')
 app.use('/threejs-experiments', staticDir)
 app.use(staticDir)
-app.listen(port, () => console.log(`Listening on port ${port}.`))
+
+const port = 5000
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`)
+  open('http://localhost:5000')
+})
