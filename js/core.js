60e393e63ccbcd2838dcc1218ab9170cdbb6984f
Micheal Parks <michealparks1989@gmail.com>
2020-08-31 19:59:34 -0400

    Updates

--- js/core.js
+++ js/core.js
@@ -1,61 +1,17 @@
 
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@v0.120.0/build/three.module.js'
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@v0.120.0/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@v0.120.0/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@v0.120.0/examples/jsm/controls/OrbitControls.js'
 import { COLORS } from './constants.js'
 
-export { THREE }
-
-const loadingManager = new THREE.LoadingManager()
-const gltfLoader = new GLTFLoader()
-const textureLoader = new THREE.TextureLoader()
-
-export const loadGLSL = async (src) => {
-  const response = await window.fetch(src)
-  const glsl = await response.text()
-  return glsl
+export {
+  THREE,
+  GLTFLoader,
+  OrbitControls
 }
 
-export const loadTexture = (src) => {
-  return textureLoader.loadAsync(src)
-}
-
-export const loadGLTF = (src) => {
-  return gltfLoader.loadAsync(src)
-}
-
-export const loadModel = async (src, config = {}) => {
-  const {
-    shadows = true,
-    matrixAutoUpdate = false,
-    anisotropy = 0
-  } = config
-
-  const model = await loadGLTF(src)
-  const { scene } = model
-
-  scene.traverse((node) => {
-    if (shadows) {
-      node.castShadow = true
-      node.receiveShadow = true
-    }
-
-    if (matrixAutoUpdate === false) {
-      node.matrixAutoUpdate = false
-      node.updateMatrix()
-    }
-
-    // TODO: We should only set anisotropy on elements that
-    // are flagged to have textures that will benefit from it.
-    if (anisotropy !== 0 && node.material !== undefined) {
-      node.material.map.anisotropy = anisotropy
-    }
-  })
-
-  return scene
-}
-
-export const createRenderer = (canvas, antialias) => {
+export const createRenderer = (canvas) => {
+  const antialias = true
   const powerPreference = 'high-performance'
   const renderer = new THREE.WebGLRenderer({
     canvas,
---
+++ js/loaders.js
@@ -0,0 +1,79 @@
+import { THREE, GLTFLoader } from './core.js'
+
+export const gltfManager = new THREE.LoadingManager()
+export const textureManager = new THREE.LoadingManager()
+
+const gltfLoader = new GLTFLoader(gltfManager)
+const textureLoader = new THREE.TextureLoader(textureManager)
+
+export const loadGLSLs = async (srcs) => {
+  const promises = []
+  for (const src of srcs) promises.push(loadGLSL(src))
+  return Promise.all(promises)
+}
+
+export const loadGLSL = async (src) => {
+  const response = await window.fetch(src)
+  const glsl = await response.text()
+  return glsl
+}
+
+export const loadTextures = async (srcs, onProgress) => {
+  const promises = []
+  textureManager.onProgress = onProgress
+  for (const src of srcs) promises.push(loadTexture(src))
+  return Promise.all(promises)
+}
+
+export const loadTexture = (src) => {
+  return textureLoader.loadAsync(src)
+}
+
+export const loadGLTFs = (srcs, onProgress) => {
+  const promises = []
+  gltfManager.onProgress = onProgress
+  for (const src of srcs) promises.push(loadGLTF(src))
+  return Promise.all(promises)
+}
+
+export const loadGLTF = (src) => {
+  return gltfLoader.loadAsync(src)
+}
+
+export const loadModels = async (srcs, config = {}, onProgress) => {
+  const promises = []
+  gltfManager.onProgress = onProgress
+  for (const src of srcs) promises.push(loadModel(src, config))
+  return Promise.all(promises)
+}
+
+export const loadModel = async (src, config = {}) => {
+  const {
+    shadows = true,
+    matrixAutoUpdate = false,
+    anisotropy = 0
+  } = config
+
+  const model = await loadGLTF(src)
+  const { scene } = model
+
+  scene.traverse((node) => {
+    if (shadows) {
+      node.castShadow = true
+      node.receiveShadow = true
+    }
+
+    if (matrixAutoUpdate === false) {
+      node.matrixAutoUpdate = false
+      node.updateMatrix()
+    }
+
+    // TODO: We should only set anisotropy on elements that
+    // are flagged to have textures that will benefit from it.
+    if (anisotropy !== 0 && node.material !== undefined) {
+      node.material.map.anisotropy = anisotropy
+    }
+  })
+
+  return scene
+}
