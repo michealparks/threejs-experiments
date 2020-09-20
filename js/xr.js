e619599e5082e69376998b7e523accb6e0f90d4e
Micheal Parks <michealparks1989@gmail.com>
2020-09-20 13:33:35 -0400

    Updates

--- js/xr.js
+++
@@ -1,8 +0,0 @@
-import { VRButton } from './core.js'
-
-export const initXR = async ({ renderer }) => {
-  if (await navigator.xr.isSessionSupported('immersive-vr')) {
-    renderer.xr.enabled = true
-    document.body.appendChild(VRButton.createButton(renderer))
-  }
-}
