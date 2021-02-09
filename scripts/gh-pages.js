a5a5b33bf7407279c9431ee53adf8941fdc7fd67
Micheal Parks <michealparks1989@gmail.com>
2021-02-09 11:17:18 -0500

    relative path bugfix

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,7 +1,9 @@
 const ghpages = require('gh-pages')
 const dir = 'build'
 
-ghpages.publish(dir, (err) => {
+ghpages.publish(dir, {
+  dotfiles: true
+}, (err) => {
   if (err) console.error(err)
   else console.log('Published')
 })
