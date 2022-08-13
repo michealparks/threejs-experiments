cf2f38cdfcedbe28a0d8dfbb3ee7ff4697d5c166
michealparks <michealparks1989@gmail.com>
2022-08-13 21:27:28 +0000

    Deploying to gh-pages from @ michealparks/threejs-experiments@05fe0718438d8a56251f5f60c95e2668134bb540 🚀

--- _app/immutable/chunks/util-three-b3683b89.js
+++
@@ -1,1 +0,0 @@
-import{M as e,w as a,v as s,x as n,b as o,c as w}from"./camera-ea5257fe.js";const r=(n=1,o=4500104)=>new e(new a(n,n,n),new s({color:o})),c=(a=1,s=16711680)=>new e(new n(a,16,16),new o({color:s})),t=(a=80,o=12)=>{const r=new s({color:2733814}),c=new n(a,o,o);return r.side=w,new e(c,r)};export{r as a,c as b,t as c};
